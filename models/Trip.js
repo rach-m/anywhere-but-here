ad435ae93c37b1a4c011f13a779b7057222913a7
Alvin Chun <alvinchun@ool-ad02017b.dyn.optonline.net>
2018-06-21 09:57:07 -0400

    Trip model added

---
+++ models/Trip.js
@@ -0,0 +1,31 @@
+const db = require('../database/connection');
+
+const Trip = {};
+
+Trip.create = newTrip => {
+  return db.one(`INSERT into trips
+  (budget, departure_date, duration, airport_id)
+  VALUES ($<budget>, $<departure_date>, $<duration>, $<airport_id>) RETURNING *`, newTrip);
+};
+
+Trip.all = () => {
+  return db.any('SELECT * FROM trips');
+}
+
+Trip.find = id => {
+  return db.one("SELECT * FROM trips WHERE trip_id = $<id>", { id: id });
+};
+
+Trip.update = UpdateTrip => {
+  return db.none(`UPDATE trips SET
+    budget = $<budget>,
+    departure_date = $<departure_date>,
+    duration = $<duration>,
+    airport_id = $<airport_id>`, UpdateTrip);
+};
+
+Trip.delete = id => {
+  return db.result("DELETE FROM trips WHERE trip_id = $<id>", { id: id });
+};
+
+module.exports = Trip;
