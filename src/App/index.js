d54c9bb3b19c9bd2f613c0f1e50d1296249af5f1
j3432p <j3432p@ool-ad020150.dyn.optonline.net>
2018-06-21 13:55:12 -0400

    create form

--- src/App/index.js
+++ src/App/index.js
@@ -43,7 +43,6 @@ class App extends Component {
             <Route path="/trips/create" exact component={CreateTripPage} />
           </div>
         </Router>;
-        <Homepage />
 
       </div>;
   }
