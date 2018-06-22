6df208067d73acfd2cb5fa5b1f655975817d5aa8
Alvin Chun <alvinchun@ool-ad02017b.dyn.optonline.net>
2018-06-22 09:47:21 -0400

    Working on Summarypage CSS

--- src/Summarypage/index.js
+++ src/Summarypage/index.js
@@ -4,17 +4,21 @@ import "./style.css";
 
 class Summarypage extends Component {
   render() {
-    return <div className="wrapper">
+    return <div className="Summarypage">
+        <header>
+          <nav>HERE IS A NAV!</nav>
+        </header>
         <h1>Summary</h1>
         <div className="information-box">
           <div className="information">
-            <img className="destination"src= "https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1508524504/paris-ROOFTOP1017.jpg?itok=arOAqg7r" />
+            <img className="destination" src="https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1508524504/paris-ROOFTOP1017.jpg?itok=arOAqg7r" />
             <p>destination: MHT</p>
             <p> departure_date: 2018-06-29</p>
             <p> return_date: 2018-07-04</p>
             <p> price: 211.40</p>
           </div>
         </div>
+        <footer>this is a footer</footer>
       </div>;
   }
 }
