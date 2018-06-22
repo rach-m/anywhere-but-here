450f2a6dfa77ba66b1e86e0a51fa20430007da4e
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2018-06-22 12:29:54 -0400

    create page redirect to show page

--- src/Summarypage/index.js
+++ src/Summarypage/index.js
@@ -6,17 +6,26 @@ class Summarypage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newTrip: []
+      id: '',
+      city_code: '',
+      departure_date: '',
+      duration: '',
+      budget: ''
     };
   }
 
   componentDidMount() {
-    fetch("/trips/create").then(newTrip => {
+    let id = this.props.match.params.id;
+    fetch(`/trips/${id}.json`).then(newTrip => {
       this.setState({
-        newTrip: newTrip
+        id: newTrip.id,
+        city_code: newTrip.city_code,
+        departure_date: newTrip.departure_date,
+        duration: newTrip.duration,
+        budget: newTrip.budget
       });
     });
-    console.log(this.state.newTrip);
+    console.log(this.state.duration);
     fetch(
       `https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=wuSjsq5981Vet1MPJkhu3FB4HxAABw1U&origin=${
         this.state.city_code
@@ -52,7 +61,7 @@ class Summarypage extends Component {
               className="destination"
               src="https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1508524504/paris-ROOFTOP1017.jpg?itok=arOAqg7r"
             />
-            <p>destination: MHT</p>
+            <p>destination: {this.state.destination}</p>
             <p> departure_date: 2018-06-29</p>
             <p> return_date: 2018-07-04</p>
             <p> price: 211.40</p>
