fdb24b325d9d182b7d08df4f6402ef235a91ca97
Alvin Chun <alvinchun@Alvins-MBP.fios-router.home>
2018-06-24 22:27:38 -0400

    Ive been trying to add navbar and still working on it

--- src/App/index.js
+++ src/App/index.js
@@ -5,22 +5,42 @@ import CreateTripPage from "../CreateTripPage";
 import Summarypage from "../Summarypage";
 import EditPage from "../EditPage";
 import Alltripspage from "../Alltripspage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const NavigationBar = ({ visible }) => (
+  <div id="navbar" className={visible ? "slideIn" : "slideOut"}>
+    Hello
+  </div>
+);
 
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { visible: false };
+  }
 
+  handleClick() {
+    this.setState(prev => ({ visible: !prev.visible }));
+  }
   render() {
     return <div>
+        <div id="App">
+          <button type="button" onClick={this.handleClick}>
+            {this.state.visible ? "Hide navbar" : "Show navbar"}
+          </button>
+          <hr />
+          <NavigationBar visible={this.state.visible} />
+        </div>
         <Router>
           <div>
             <Route path="/" exact component={Homepage} />
             <Route path="/trips/create" exact component={CreateTripPage} />
             <Route path="/trips/:id.json" exact component={Summarypage} />
             <Route path="/trips/:id/edit" exact component={EditPage} />
-            <Route path="/trips" exact component={Alltripspage} />
+            <Route path="/trips.json" exact component={Alltripspage} />
           </div>
         </Router>
       </div>;
